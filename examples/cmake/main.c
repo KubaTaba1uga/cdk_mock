55fe8df9f163f2c5cf86ba3abf11311dd1cc72c9
KubaTaba1uga <luna7337anul@proton.me>
2025-04-11 06:20:11 +0200

    fix all cmake example bugs

--- examples/cmake/main.c
+++ examples/cmake/main.c
@@ -3,16 +3,17 @@
 #include "lib.h"
 #include "main.h"
 
-/* #include <c_minilib_mock.h> */
+#include <c_minilib_mock.h>
 
-/* MOCKABLE(int bar(void)) { return 89; } */
-/* MOCKABLE_DUPLICATE(bar); */
+MOCKABLE(int bar(void)) { return 888; }
+MOCKABLE_DUPLICATE(bar);
 
 #ifdef DEBUG
 int _main(void) {
 #else
 int main(void) {
 #endif
+  printf("bar returned: %d\n", bar());
   printf("foo returned: %d\n", foo());
   return 0;
 }
--- examples/cmake/test_main.c
+++ examples/cmake/test_main.c
@@ -2,6 +2,13 @@
 
 // mock implementation
 int __wrap_foo(void) { return 999; }
+int bar_orig(void);
+
+int bar(void) {
+  puts("Mocked called");
+  return bar_orig();
+}
+
 int _main(void);
 
 void test_main() { _main(); }
