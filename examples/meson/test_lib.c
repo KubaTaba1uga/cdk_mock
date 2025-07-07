8fd93d82d0ff6962dc1a82b35be4c9b278df9469
KubaTaba1uga <luna7337anul@proton.me>
2025-07-07 11:43:37 +0200

    fix bug

--- examples/meson/test_lib.c
+++ examples/meson/test_lib.c
@@ -3,11 +3,7 @@
 
 #include "lib.h"
 
-int __wrap_foo() { return 999; }
-int bar() { return 111; }
-
 int main(void) {
-
   assert(111 == bar());
   printf("bar returned: %d\n", bar());
   assert(999 == foo());
--- examples/meson/test_main.c
+++ examples/meson/test_main.c
@@ -1,16 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
 
-// mock implementation
-int __wrap_foo(void) { return 999; }
-int bar_orig(void);
+#include "lib.h"
 
-int bar(void) {
-  puts("Mocked called");
-  return bar_orig();
+int __wrap_foo() { return 999; }
+int bar() { return 111; }
+
+int main(void) {
+
+  assert(111 == bar());
+  printf("bar returned: %d\n", bar());
+  assert(999 == foo());
+  printf("foo returned: %d\n", foo());
+
+  return 0;
 }
-
-int _main(void);
-
-void test_main() { _main(); }
-
-int main() { test_main(); }
