79b3da0165039b61be928ad689b2c5cb1d183054
KubaTaba1uga <luna7337anul@proton.me>
2025-08-28 14:49:49 +0200

    add wrap example to gcc

--- examples/mockable_macro/compile.sh
+++ examples/mockable_macro/compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xeu
 
-
 # Get absolute path to the script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -12,16 +11,27 @@ cd build
 
 
 # List of source files (one per binary)
-sources=("example_1.c" "example_2.c")
+sources=("example_1.c" "example_2.c" "example_3.c")
 
 for src in "${sources[@]}"; do
   idx="$(basename "$src" .c | sed 's/example_//g')"  # e.g., "1"
   src_path="$SCRIPT_DIR/$src"
+  lib_path="$SCRIPT_DIR/lib_$idx.c"
   test_src_path="$SCRIPT_DIR/mock_$idx.c"
 
-  # Normal build
-  gcc -I../../../include "$src_path" -o "example_$idx"
+  if [ ! -f "$lib_path" ]; then
+    # Normal build
+    gcc -I../../../include "$src_path" -o "example_$idx"
 
-  # Mocked build
-  gcc -I../../../include -DCDK_MOCK_ENABLE "$src_path" "$test_src_path" -o "example_${idx}_test"
+    # Mocked build
+    gcc -I../../../include -DCDK_MOCK_ENABLE "$src_path" "$test_src_path" -o "example_${idx}_test"
+      
+  else
+    # Normal build
+    gcc -I../../../include "$src_path" "$lib_path" -o "example_$idx"
+
+    # Mocked build
+    gcc -I../../../include -DCDK_MOCK_ENABLE -Wl,--wrap=add "$src_path" "$lib_path" "$test_src_path" -o "example_${idx}_test"
+  fi
+    
 done
